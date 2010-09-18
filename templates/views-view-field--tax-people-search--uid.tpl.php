8a8581a93ea7044d142869dfc67f4cc6874acc97
William Wolfrath <william.wolfrath@gmail.com>
2010-09-17 20:18:21 -0500

    Get university from taxonomy; improved display of user info

---
+++ templates/views-view-field--tax-people-search--uid.tpl.php
@@ -0,0 +1,47 @@
+<?php
+// $Id: views-view-field.tpl.php,v 1.1 2008/05/16 22:22:32 merlinofchaos Exp $
+ /**
+  * This template is used to print a single field in a view. It is not
+  * actually used in default Views, as this is registered as a theme
+  * function which has better performance. For single overrides, the
+  * template is perfectly okay.
+  *
+  * Variables available:
+  * - $view: The view object
+  * - $field: The field handler object that can process the input
+  * - $row: The raw SQL result that can be used
+  * - $output: The processed output that will normally be used.
+  *
+  * When fetching output from the $row, this construct should be used:
+  * $data = $row->{$field->field_alias}
+  *
+  * The above will guarantee that you'll always get the correct data,
+  * regardless of any changes in the aliasing that might happen if
+  * the view is modified.
+  */
+?>
+<?php //dd($output); ?>
+<?php
+        $profile_user = user_load($output);
+        $fbid = $profile_user->facebook_id;
+        if (!$fbid) { $fbid = '0'; }  // for some crazy reason which I have yet to discover, omitting this causes all fp:profile-pic calls to return a silhouette. in some browsers. in some views. odd.
+        //print '<div class="facebook-picture"><fb:profile-pic uid="' . $fbid . '"  size="normal" width="100" facebook-logo="true" linked="false"></fb:profile-pic></div>';
+?>
+<div class="facebook-picture">
+<?php print theme('kosmos_user_picture', $profile_user, $fbid, 100); ?>
+</div>
+
+<div class="people-search-user-info">
+<?php
+$profile_node = content_profile_load('profile', $output);
+//dsm($profile_node);
+$college = taxonomy_get_term($profile_node->field_university_tax[0]['value']);
+//dsm($profile_node->field_university_tax[0]['value']);
+//dsm($college);
+$user_info = ($college->name) ? ', ' . $college->name : '';
+$user_info .= ($profile_node->field_academic_discipline[0]['value']) ? ', ' . $profile_node->field_academic_discipline[0]['value'] : '';
+print '<h3>' . l($profile_user->name, "user/$output") . '</h3>' . $profile_user->user_type . $user_info ;
+?>
+</div>
+<div class="clear">&nbsp;</div>
+<?php //print $output; ?>
